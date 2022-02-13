4d6392e04bfc9374257fdab79fc8bc7b53a9dbf2
khodges <zkellyhodges@gmail.com>
2022-02-13 11:40:16 -0800

    added some more options for patrol function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let slays = [];
 let playerItems = ["cell-phone", "senor pointy"];
 let city ;
 let location;
+let watcher ;
 
 const prompt = require('prompt-sync')();
 const username = prompt('Hiya, what is your name?');
@@ -40,10 +41,53 @@ function toPatrolOrToNot(){
             console.log("The vamp goes in for the bite but you are too quick and you save the poor sap who thought they had met someone nice for once.");
             console.log("The poor sap, Gene, is now your friend.");
             friends.push("Gene");
-        }else if(toSaveOrNot === ["P"]){
+        }else if(toSaveOrNot === "P"){
             console.log("The vamp goes in for the bite and your Watcher tries to intervene. Oops now the poor sap who was with the vamp is dead and your Watcher is too!")
             console.log("Your heart is now broken...");
-            playerHealth = 0;
+            watcher === false;
+            if(friends.length > 0){
+                console.log(`Luckily you have friends...`)
+                displayFriends()
+                console.log("Your friend boosts your mental health and you remember there is a Watchers Council you can track down.");
+                console.log("Now you must journey to find a new Watcher\n")
+                const goToWatchersCouncil = prompt (`Do you [G] go to Watchers Council or [N] not?`)
+                if(goToWatchersCouncil === "G"){
+                    console.log("Good choice. You can't do this without guidance. You're just a baby slayer, after all.")
+                    console.log("The council appoints you a new Watcher, Wesley")
+                    watcher === true;
+                }else if(goToWatchersCouncil === "N"){
+                    console.log("You're going rogue eh?\n")
+                    console.log("Good luck with that!")
+                    console.log("You are trying to make it back to your hotel but are low on cash and food, your health is waning and you just lost a friend.")
+                    friends.pop()
+                    console.log("As you're dumpster diving, you notice someone is watching you...\n")
+                    console.log("You think you'll have to pull out your slaying powers but you're lucky this time.\n")
+                    console.log("It is the bad vamp turned good, Spike. You are too desperate to turn his help down.")
+                    friends.push("Spike");
+                    console.log("Spike gets you some food and lets you know that the Watchers council will be sending you a new Watcher, Wesley.\n")
+                    playerHealth =+ 10;
+                    watcher === true;
+
+                }
+            }else if(friends.length = 0){
+                console.log("A friend would really come in handy...\n")
+                console.log("Looks like you don't have any :(")
+                displayFriends()
+                console.log("You need to find help fast");
+                const findFriend = prompt (`Luck for you someone is lurking in the shadows...do you [A] approach or [R] run from the shadow lurker?`)
+                if(findFriend === "A"){
+                    console.log("Lucky for you, it is the bad vamp turned good, Spike!")
+                    friends.push("Spike");
+                    console.log("Spike helps link you up with the Watchers council and your now you have him and yourWacther Wesley is here to help you.");
+                }else if(findFriend === "R"){
+                    console.log("Nice try. It is the bad vamp turned good, Spike. He's got hundreds of years on you and he outruns you.");
+                    console.log("He doesn't scare easily and is there to help whether you want it or not.\n")
+                    friends.push("Spike");
+                    console.log("He lets you know that the Watchers council will be sending you a new Watcher, Wesley.")
+                    watcher === true;
+
+                }
+            }
         }
     
     } 
@@ -71,21 +115,23 @@ function boogleMaps(){
         console.log("Look at you, a regular Bend, Oregon Buffy!")
         console.log("You immediately find your Watcher, Hermes von Giles frantically fumbling with books and is relieved when he notices you there.")
         console.log("'Oh my there you are!'")
-        friends.push("Hermes von Giles");
+        watcher === true;
     }else if(mapChoice === "W" && city === "Bend"){
         console.log("I don't blame you. Being a slayer is going to be tough.");
         console.log("Oh snap! Your new Watcher, Hermes von Giles, is a wine lover too! You guys bond over some funky red wine. Good times, but it gives you a headache!")
-        friends.push("Hermes von Giles");
+        watcher === true;
         playerHealth -= 5;
     }else if(mapChoice === "L" && city === "Sunnydale"){
         console.log("Smart choice. You arrive at the library, see good old Giles there.")
         console.log("He is happy to see you. He feeds you, gives you some lunch, yum thanks G.")
+        watcher === true;
         playerHealth += 10;
     }else if(mapChoice === "W" && city === "Sunnydale"){
         console.log("Well a winery in Sunnydale isn't your normal haunt...");
         console.log(`Your enemy ${enemies[0]} is there in a murderous rampage, seeking revenge on you but hurting others.`);
         console.log(`You guys battle, you take a hit but ultimately you slay the demon.`)
         console.log("Your new watcher finds you and is happy to see you're already at work.")
+        watcher === true;
         playerHealth -= 10;
         slays.push("Gragnok");
         enemies.pop();
